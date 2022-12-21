f66bd44d17d0c39d0a6ef3d1bf129b02dc16044e
“Shawna <shawna.e.hartley@gmail.com>
2022-12-21 16:06:12 -0500

    updated user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
+
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  describe 'uniqueness' do 
+    before :each do 
+      create(:user)
+    end
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:session_token) }
+  end
+
+
+  # users can sign up
+  # users can log in
+  # users can log out
+  # users make goals
+  # users can post comments on profiles (other users)
+  # users can post comments on goals (thier own or others')
+
+
 end
